4d93dbac225f93162b574a5b8b0afd66efc6d496
jotap1101 <joaopedroalves.j1101p@gmail.com>
2025-04-08 16:46:03 -0300

    Refactor user URLs

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('auth/register/', RegisterView.as_view(), name='register'),
 
-    path('auth/account/<uidb64>/<token>/activate/', ActivateAccountView.as_view(), name='activate'),
-    path('auth/account/activate/done/', RegisterDoneView.as_view(), name='register_done'),
-    path('auth/account/profile/', AccountProfileView.as_view(), name='profile'),
-    path('auth/account/security/', AccountSecurityView.as_view(), name='settings'),
+    path('account/activate/<uidb64>/<token>/', ActivateAccountView.as_view(), name='activate'),
+    path('account/activate/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('account/profile/', AccountProfileView.as_view(), name='profile'),
+    path('account/security/', AccountSecurityView.as_view(), name='settings'),
 
-    path('auth/password_reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('auth/password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('auth/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('auth/reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password/reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password/reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
